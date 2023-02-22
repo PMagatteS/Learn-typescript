6baee339fbf94ab31260f945c8fdf389f16ee401
PMagatteS <papamagattesady@gmail.com>
2023-02-22 23:08:54 +0000

    Learned about variables declaration and types

---
+++ types.ts
@@ -0,0 +1,32 @@
+// Defining Types
+
+let str: string;
+str = "String value";
+let num: number;
+num = 23;
+let bool: boolean;
+bool = true
+let arr: (number|string)[] = [];
+arr = [2, "An array with a string annd a number"]
+let obj: {name: string, age: number};
+// obj = {name: "john", age: 48}
+
+
+
+// Types by Inference
+
+let str2: "String value";
+let num2: 23;
+let bool2: true;
+let arr2: [2, "An array with a string annd a number"];
+let obj2: {name: "jonh", age: 48};
+
+
+// Any type
+
+let anything: any
+anything = 34;
+anything = "A string";
+anything = false;
+anything = [12, "Another string"];
+anything= {name: "john", age: 34}
