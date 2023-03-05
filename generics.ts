eb5e767c6f2e326f403855826341f0111e48658d
PMagatteS <papamagattesady@gmail.com>
2023-03-05 21:03:02 +0000

    Not done with generics yet

--- generics.ts
+++ generics.ts
@@ -4,7 +4,8 @@
 
 // Classic functions
 
-function genericAdd<T extends number>(data1:T, data2:T){
+// We can constrain a generic to one or multiple types  with the keyword extends
+function genericAdd<T extends number|string|(string|number)[]>(data1:T, data2:T){
 
     if(typeof data1 ==="number" && typeof data2 ==="number"){
         console.log(`The sum of the values is : ${data1+data2}`);
@@ -21,7 +22,36 @@ function genericAdd<T extends number>(data1:T, data2:T){
     return [data1, data2]
 }
 
+genericAdd(12, 45)
+genericAdd('Hello ', 'world')
+genericAdd([1,2,3], [4,5,6])
+
+
 // Arrow Functions
 const genericArray = <T, U> (data1:T, data2:T) :(T|U)[] => {
     return[data1, data2]
 }
+
+// Aliases generics
+
+type Something<T>= T
+type ObjListThing<T>=  T[]
+
+
+const word : Something<string> = "Word"
+const numb : Something<number> = 123
+const listOfNums : ObjListThing<number> = [1, 2, 3]
+const listOfBools : ObjListThing<boolean> = [true, false]
+// Classes generics
+
+class Person<T=string> {
+    private _info: T | undefined;
+  
+    constructor(private name: string) {}
+    set info(values:T){
+        this._info= values
+    }
+
+  }
+const john = new Person("John")
+john.info = "A anonymous person"
